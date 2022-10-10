f1a2779ed1ad501e0e1a416ead4ca4fb1ace4f03
mathtijs <matthijsjurriaans333@gmail.com>
2022-10-10 19:57:53 +0200

    week 18 af minus darkmode

--- week 4/js/script.js
+++ week 4/js/script.js
@@ -1,17 +1,25 @@
+
+/* javascript inputchange*/
 const output1 = document.getElementById("js--output1")
 const output2 = document.getElementById("js--output2")
 const output3 = document.getElementById("js--output3")
 
 const input = document.getElementById("js--input")
 
-console.log(output1)
-console.log(output2)
-console.log(output3)
-
-console.log(input)
-
 input.oninput = function(event){
     output1.innerText = event.target.value
     output2.innerText = event.target.value
     output3.innerText = event.target.value
+}
+
+/* javascript voor favorites*/
+const hearts = document.getElementsByClassName("heart__logo");
+
+for(let i = 0; i < hearts.length; i++){
+    hearts[i].onclick = function(){
+        for(let i = 0; i < hearts.length; i++){
+            hearts[i].classList = "fa-regular fa-heart heart__logo"
+        }
+        hearts[i].classList = "fa-solid fa-heart heart__logo";
+    }
 }
